4b03c574bface1532a9eb5c3bd1b5e1e39277ce1
chenbo <shadowboc0302@gmail.com>
2024-10-27 21:44:20 +0800

    add code

---
+++ chapter/share_mem_use/ipc_utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*
+int ipc_server_init(struct ipc_mem_cfg* cfg, uint8_t channel_id);
+
+uint32_t* ipc_server_alloc_one_buf(uint8_t channel_id);
+
+int32_t ipc_server_put_one_buf(uint8_t channel_id, uint32_t* buf, uint32_t size);
+
+int ipc_client_init(struct ipc_mem_cfg* cfg, uint8_t channel_id);
+
+uint32_t* ipc_client_get_one_buf(uint8_t channel_id, uint32_t* size);
+
+void ipc_client_free_one_buf(uint8_t channel_id, uint32_t* buf);
+
+*/
